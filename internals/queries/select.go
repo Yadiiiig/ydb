689c11caa6a5b093899d2a508d653667d1ed7926
yadiiiig <willemteughels@gmail.com>
2021-07-01 18:57:20 +0200

    Optimized select query x4 times faster

--- internals/queries/select.go
+++ internals/queries/select.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"sync"
 
 	pb "github.com/Yadiiiig/ydb/internals/proto"
 	"github.com/Yadiiiig/ydb/internals/reader"
@@ -13,7 +14,28 @@ import (
 
 func Select(d *reader.Drivers, in *pb.SelectValues) string {
 	result := []interface{}{}
-	for _, v := range d.Database[in.GetTable()] {
+	amount := len(d.Database[in.GetTable()])
+	var wg sync.WaitGroup
+
+	for i := 0; amount > 0; i += 1000 {
+		amount -= 1000
+		if amount > 0 {
+			wg.Add(1)
+			go selector(d, in, &result, i, i+1000, &wg)
+		} else {
+			tmp := 1000 + amount
+			wg.Add(1)
+			go selector(d, in, &result, i, i+tmp, &wg)
+		}
+	}
+	wg.Wait()
+	r, _ := json.Marshal(result)
+	return string(r)
+}
+
+func selector(d *reader.Drivers, in *pb.SelectValues, result *[]interface{}, x, y int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, v := range d.Database[in.GetTable()][x:y] {
 		tempBool := true
 		for _, vq := range in.GetValues() {
 			if !utils.OperatorQuery(reflect.ValueOf(v).Elem().FieldByName(strings.Title(vq.Row)).String(), vq.Value, vq.Operator) {
@@ -23,16 +45,12 @@ func Select(d *reader.Drivers, in *pb.SelectValues) string {
 		}
 		if tempBool {
 			if len(in.GetFields()) == 1 && in.GetFields()[0] == "*" {
-				result = append(result, v)
+				*result = append(*result, v)
 			} else {
-				result = append(result, appendResult(v, in.GetFields()))
+				*result = append(*result, appendResult(v, in.GetFields()))
 			}
 		}
 	}
-
-	r, _ := json.Marshal(result)
-
-	return string(r)
 }
 
 func appendResult(value interface{}, fields []string) interface{} {
